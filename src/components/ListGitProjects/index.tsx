76875069c32b187d59b355c12cf2ffd5c47724e5
Mynameisjohndev <joaoantonio15161516@gmail.com>
2023-06-20 23:18:59 -0300

    feat: card list component

--- src/components/ListGitProjects/index.tsx
+++ src/components/ListGitProjects/index.tsx
@@ -1,27 +1,39 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { motion, AnimatePresence } from 'framer-motion';
 import { useState } from 'react';
+import { AiFillFolder, AiFillGithub } from 'react-icons/ai';
 
 import { ContainerList, ListGitProjectsContainer } from './styles';
 
 const cardVariants = {
-  hidden: { opacity: 0, y: 50 },
-  show: { opacity: 1, y: 0 },
-  exit: { opacity: 0, y: -50 },
+  // hidden: { opacity: 0, y: 50 },
+  // show: { opacity: 1, y: 0 },
+  // exit: { opacity: 0, y: -50 },
 };
 
 const ListGitProjects = () => {
-  const [items, setItems] = useState([1, 2, 3, 4]);
+  // const [items, setItems] = useState([1, 2, 3, 4, 5, 6, 7, 8]);
+  const [cards, setCards] = useState([
+    { title: 'Card 1', description: 'Description 1' },
+    { title: 'Card 2', description: 'Description 2' },
+    { title: 'Card 3', description: 'Description 3' },
+    { title: 'Card 4', description: 'Description 4' },
+    { title: 'Card 5', description: 'Description 5' },
+    { title: 'Card 6', description: 'Description 6' },
+    { title: 'Card 7', description: 'Description 7' },
+    { title: 'Card 8', description: 'Description 8' },
+  ]);
 
   const addItem = () => {
-    setItems([...items, Math.max(...items) + 1]);
+    // setCards([...cards, Math.max(...cards) + 1]);
   };
 
-  const removeItem = (index: number) => {
-    setItems(items.filter((item, i) => i !== index));
-  };
+  // const removeItem = (index: number) => {
+  //   setCards(cards.filter((cards, i) => i !== index));
+  // };
 
   const sortItems = () => {
-    setItems([...items].sort((a, b) => b - a));
+    // setCards([...cards].sort((a, b) => b - a));
   };
 
   return (
@@ -30,21 +42,43 @@ const ListGitProjects = () => {
       <button onClick={sortItems}>Ordenar Itens</button>
       <AnimatePresence>
         <ListGitProjectsContainer>
-          {items.map((item, index) => (
-            <motion.div
-              key={item}
-              variants={cardVariants}
-              initial={{ opacity: 0, scale: 0 }}
-              animate={{ opacity: 1, scale: 1 }}
-              exit={{ opacity: 0, scale: 0 }}
-              layout
-              layoutDependency={{ type: 'spring', stiffness: 500, damping: 30 }}
-              className="card"
-            >
-              <p>{item}</p>
-              <button onClick={() => removeItem(index)}>Remover</button>
-            </motion.div>
-          ))}
+          {cards.map((card, index) => {
+            const { title } = card;
+            return (
+              <motion.div
+                key={index}
+                variants={cardVariants}
+                initial={{ opacity: 0, scale: 0 }}
+                animate={{ opacity: 1, scale: 1 }}
+                exit={{ opacity: 0, scale: 0 }}
+                layout
+                layoutDependency={{
+                  type: 'spring',
+                  stiffness: 500,
+                  damping: 30,
+                }}
+                className="card"
+              >
+                <div className="column">
+                  <div className="title">
+                    <AiFillFolder />
+                    <AiFillGithub className="showingit" />
+                  </div>
+                  <div className="content">
+                    <h1>{title}</h1>
+                    <p>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Quibusdam nihil autem optio.
+                    </p>
+                  </div>
+                </div>
+                <div className="footer">
+                  <span>React</span>
+                  <span>Typescript</span>
+                </div>
+              </motion.div>
+            );
+          })}
         </ListGitProjectsContainer>
       </AnimatePresence>
     </ContainerList>
--- src/components/ListGitProjects/styles.ts
+++ src/components/ListGitProjects/styles.ts
@@ -1,9 +1,10 @@
 import { motion } from 'framer-motion';
+import { transparentize } from 'polished';
 import styled from 'styled-components';
 
 const ContainerList = styled.div`
   align-self: center;
-  transition: 0.3s all;
+  /* transition: 0.3s all; */
 
   button {
     background-color: ${({ theme }) => theme.COLORS.GREEN_30};
@@ -13,34 +14,86 @@ const ContainerList = styled.div`
 const ListGitProjectsContainer = styled(motion.div)`
   display: grid;
   grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
-  grid-gap: 30px;
-  width: 1120px;
-  transition: 0.3s all;
+  grid-gap: 10px;
+  width: 1030px;
+  /* transition: 0.3s all; */
   @media (max-width: 1220px) {
-    width: 840px;
+    width: 770px;
+    /* width: 840px; */
   }
   @media (max-width: 950px) {
-    width: 560px;
+    width: 510px;
+    /* width: 560px; */
   }
   @media (max-width: 660px) {
-    width: 280px;
+    width: 250px;
   }
 
   .card {
+    /* border: 0.1px solid
+      ${({ theme }) => transparentize(0.9, theme.COLORS.WHITE)}; */
     margin: auto;
     width: 250px;
     background-color: ${({ theme }) => theme.COLORS.BLACK_60};
     height: 250px;
     display: flex;
-    justify-content: center;
-    align-items: center;
+    justify-content: space-between;
+    flex-direction: column;
     border-radius: 0.4rem;
-    transition: 0.3s all;
+    padding: 1rem;
+    .column {
+      display: flex;
+      flex-direction: column;
+    }
+    .title {
+      display: flex;
+      width: 100%;
+      flex-direction: row;
+      justify-content: space-between;
+      svg {
+        width: 40px;
+        height: 40px;
+      }
+      .showingit {
+        width: 20px;
+        height: 20px;
+        cursor: pointer;
+      }
+    }
+    .content {
+      display: flex;
+      flex-direction: column;
+      width: 100%;
+      transition: 0.3s all;
+      &:hover {
+        h1 {
+          color: ${({ theme }) => theme.COLORS.GREEN_30};
+        }
+      }
+      h1 {
+        color: ${({ theme }) => theme.COLORS.WHITE};
+        font-size: 1rem;
+        font-weight: 500;
+      }
+      p {
+        font-size: 0.7rem;
+        width: 100%;
+        color: ${({ theme }) => transparentize(0.5, theme.COLORS.WHITE)};
+      }
+    }
+    .footer {
+      display: flex;
+      flex-direction: row;
+
+      span {
+        margin: 0.3rem;
+        font-size: 0.6rem;
+        color: ${({ theme }) => transparentize(0.6, theme.COLORS.WHITE)};
+      }
+    }
     &:hover {
-      /* margin-bottom: 0.4rem; */
-      transform: scaleX(1.1);
-      width: 240px;
-      height: 240px;
+      /* -webkit-box-shadow: 0px 0px 15px -4px rgba(0, 0, 0, 0.4);
+      box-shadow: 0px 0px 15px -4px rgba(0, 0, 0, 0.4); */
     }
   }
 `;
