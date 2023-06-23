0f7b1baa403c605f87eab1e98aa79762ec7a93bb
Mynameisjohndev <joaoantonio15161516@gmail.com>
2023-06-22 23:22:19 -0300

    feat: adding titles

--- src/pages/Portfolio/Projects/index.tsx
+++ src/pages/Portfolio/Projects/index.tsx
@@ -7,9 +7,9 @@ import { ContainerProjects } from './styles';
 const Projects = () => {
   return (
     <ContainerProjects>
-      {/* <h1>Algumas coisas que desenvolvi</h1> */}
+      <h1>Algumas coisas que desenvolvi</h1>
       <ListProject {...{ projects: data.projects }} />
-      {/* <h1>Outros projetos notáveis</h1> */}
+      <h1>Outros projetos notáveis</h1>
       <ListGitProjects {...{ projects: datagit.projects }} />
     </ContainerProjects>
   );
